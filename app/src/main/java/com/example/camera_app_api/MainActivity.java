881eaaddaf023e4857dbb5d94151d3a3cf102211
Tatathinking <66949045+Tatathinking@users.noreply.github.com>
2020-06-22 11:13:30 +0800

    Basic camera base on cameraX

---
+++ app/src/main/java/com/example/camera_app_api/MainActivity.java
@@ -0,0 +1,168 @@
+package com.example.camera_app_api;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageAnalysisConfig;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureConfig;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.camera.core.CameraX;
+import androidx.lifecycle.LifecycleOwner;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Matrix;
+import android.graphics.Point;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.util.Rational;
+import android.util.Size;
+import android.view.Display;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    int REQUEST_CODE_PERMISSIONS = 10; //arbitrary number, can be changed accordingly
+    final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA","android.permission.WRITE_EXTERNAL_STORAGE"};
+    TextureView txtureView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtureView = findViewById(R.id.view_finder);
+        for(String permission : REQUIRED_PERMISSIONS){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+            }
+        }
+        startCamera();
+    }
+
+    private void startCamera() {
+        //make sure there isn't another camera instance running before starting
+        CameraX.unbindAll();
+
+        /* start preview */
+        int aspRatioW = txtureView.getWidth(); //get width of screen
+        int aspRatioH = txtureView.getHeight(); //get height
+        Rational asp = new Rational (aspRatioW, aspRatioH); //aspect ratio
+        Size screen = new Size(aspRatioW, aspRatioH); //size of the screen
+
+        //config obj for preview/viewfinder thingy.
+        PreviewConfig pConfig = new PreviewConfig.Builder().setTargetAspectRatio(asp).setTargetResolution(screen).build();
+        Preview preview = new Preview(pConfig); //lets build it
+
+        preview.setOnPreviewOutputUpdateListener(
+                new Preview.OnPreviewOutputUpdateListener() {
+                    //to update the surface texture we have to destroy it first, then re-add it
+                    @Override
+                    public void onUpdated(Preview.PreviewOutput output){
+                        ViewGroup parent = (ViewGroup) txtureView.getParent();
+                        parent.removeView(txtureView);
+                        parent.addView(txtureView, 0);
+
+                        txtureView.setSurfaceTexture(output.getSurfaceTexture());
+                        updateTransform();
+                    }
+                });
+
+        /* image capture */
+
+        //config obj, selected capture mode
+        ImageCaptureConfig imgCapConfig = new ImageCaptureConfig.Builder().setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+                .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation()).build();
+        final ImageCapture imgCap = new ImageCapture(imgCapConfig);
+
+        findViewById(R.id.capture_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File file = new File(Environment.getExternalStorageDirectory() + "/" + System.currentTimeMillis() + ".jpg");
+                imgCap.takePicture(file, new ImageCapture.OnImageSavedListener() {
+                    @Override
+                    public void onImageSaved(@NonNull File file) {
+                        String msg = "Photo capture succeeded: " + file.getAbsolutePath();
+                        Toast.makeText(getBaseContext(), msg,Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCapture.UseCaseError useCaseError, @NonNull String message, @Nullable Throwable cause) {
+                        String msg = "Photo capture failed: " + message;
+                        Toast.makeText(getBaseContext(), msg,Toast.LENGTH_LONG).show();
+                        if(cause != null){
+                            cause.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+        /* image analyser */
+
+        ImageAnalysisConfig imgAConfig = new ImageAnalysisConfig.Builder().setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE).build();
+        ImageAnalysis analysis = new ImageAnalysis(imgAConfig);
+
+        analysis.setAnalyzer(
+                new ImageAnalysis.Analyzer(){
+                    @Override
+                    public void analyze(ImageProxy image, int rotationDegrees){
+                        //y'all can add code to analyse stuff here idek go wild.
+                    }
+                });
+
+        //bind to lifecycle:
+        CameraX.bindToLifecycle((LifecycleOwner)this, analysis, imgCap, preview);
+    }
+
+    private void updateTransform(){
+        /*
+         * compensates the changes in orientation for the viewfinder, bc the rest of the layout stays in portrait mode.
+         * methinks :thonk:
+         * imgCap does this already, this class can be commented out or be used to optimise the preview
+         */
+        Matrix mx = new Matrix();
+        float w = txtureView.getMeasuredWidth();
+        float h = txtureView.getMeasuredHeight();
+
+        float centreX = w / 2f; //calc centre of the viewfinder
+        float centreY = h / 2f;
+
+        int rotationDgr;
+        int rotation = (int)txtureView.getRotation(); //cast to int bc switches don't like floats
+
+        switch(rotation){ //correct output to account for display rotation
+            case Surface.ROTATION_0:
+                rotationDgr = 0;
+                break;
+            case Surface.ROTATION_90:
+                rotationDgr = 90;
+                break;
+            case Surface.ROTATION_180:
+                rotationDgr = 180;
+                break;
+            case Surface.ROTATION_270:
+                rotationDgr = 270;
+                break;
+            default:
+                return;
+        }
+
+        mx.postRotate((float)rotationDgr, centreX, centreY);
+        txtureView.setTransform(mx); //apply transformations to textureview
+    }
+}
